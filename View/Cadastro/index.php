e3b68e98fe13633e3923b31ad689f76acfdc3a9a
Eduardo <lourenco.e@pucpr.edu.br>
2025-05-09 20:57:45 -0300

    [add] initial commit

--- View/Cadastro/index.php
+++ View/Cadastro/index.php
@@ -52,12 +52,12 @@ if (isset($data["form"])) {
   </div>
 </div>
 
-<div class="hidden z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+<div id="read-card-modal" class="hidden z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
   <div class="fixed inset-0 bg-gray-500/75 transition-opacity" aria-hidden="true"></div>
   <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
     <div class="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
       <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+        <div class="bg-white px-8 pt-5 pb-4 sm:p-6 sm:pb-4">
           <div class="flex flex-col items-center gap-2">
             <div class="mx-auto flex size-20 shrink-0 items-center justify-center rounded-full bg-gray-200">
               <i class="fa-solid fa-address-card text-primary text-4xl"></i>
@@ -70,8 +70,8 @@ if (isset($data["form"])) {
             </div>
           </div>
         </div>
-        <div class=" bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-          <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancelar</button>
+        <div class=" bg-gray-50 px-8 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+          <button onclick="closeReadCardModal()" type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-100 sm:mt-0 sm:w-auto cursor-pointer">Cancelar</button>
         </div>
       </div>
     </div>
--- View/Cadastro/script.js
+++ View/Cadastro/script.js
@@ -1,40 +1,18 @@
-let step = "step-1";
-
-const tipoUsuarioRadioButtons = document.querySelectorAll(".person-type-radio");
-
-const step1SubmitButton = document.getElementById("step1-submit-button");
-
 const form = document.getElementById("form");
+let modal;
 
-tipoUsuarioRadioButtons.forEach((radio) => {
-  radio.addEventListener("change", function () {
-    step1SubmitButton.removeAttribute("disabled");
-  });
-});
-
-function getSelectedTipoUsuario() {
-  const selectedRadio = document.querySelector('input[name="tipoUsuario"]:checked');
-  
-  if (selectedRadio) {
-    return selectedRadio.value
-  } else {
-    return null;
-  }
+function openReadCardModal() {
+  modal = document.getElementById("read-card-modal");
+  modal.classList.remove("hidden");
+  modal.classList.add("relative");
 }
 
-function nextStep(currentStep) {
-  const tipoUsuario = getSelectedTipoUsuario();
-
-  if(currentStep == 1) {
-    document.getElementById(`step-${currentStep}`).classList.add("hidden");
-  } else {
-    document.getElementById(`step-${currentStep}-${tipoUsuario}`).classList.add("hidden");
-  }
-  document
-    .getElementById(`step-${(currentStep + 1)}-${tipoUsuario}`)
-    .classList.remove("hidden");
+function closeReadCardModal() {
+  modal.classList.remove("relative");
+  modal.classList.add("hidden");
 }
 
 function submitForm() {
-  form.submit();
+  openReadCardModal();
+  //form.submit();
 }

56565a271361ae3384bf5cb1e2f91f4d09142c58
Eduardo <lourenco.e@pucpr.edu.br>
2025-05-09 20:49:57 -0300

    [add] initial commit

--- View/Cadastro/index.php
+++ View/Cadastro/index.php
@@ -51,3 +51,29 @@ if (isset($data["form"])) {
     </form>
   </div>
 </div>
+
+<div class="hidden z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+  <div class="fixed inset-0 bg-gray-500/75 transition-opacity" aria-hidden="true"></div>
+  <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+    <div class="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+      <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+          <div class="flex flex-col items-center gap-2">
+            <div class="mx-auto flex size-20 shrink-0 items-center justify-center rounded-full bg-gray-200">
+              <i class="fa-solid fa-address-card text-primary text-4xl"></i>
+            </div>
+            <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+              <h3 class="text-base font-semibold text-gray-900" id="modal-title">Aguardando a leitura do cartão...</h3>
+              <div class="pt-2">
+                <p class="text-sm text-gray-500">Aproxime o seu cartão de acesso no dispositivo de leitura ESP32 para finalizar o seu cadastro no sistema.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class=" bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+          <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancelar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>

fcf3daebcb376cfcbfc97bd638ee01587aeba407
Eduardo <lourenco.e@pucpr.edu.br>
2025-05-30 20:20:20 -0300

    [add] cadastro finalizado

--- View/Cadastro/bem-vindo/index.php
+++ View/Cadastro/bem-vindo/index.php
@@ -8,7 +8,7 @@
     <h2 class="text-2xl font-semibold">Sua conta foi criada!</h2>
 
     <p class="text-gray-700">
-      Agora você já pode realizar o login com o seu e-mail, senha e cartão!.
+      Agora você já pode realizar o login com o seu e-mail, senha e lendo o seu cartão!
     </p>
 
     <a href="/<?= BASE_DIR_NAME ?>" class="button flex flex-row items-center justify-center font-medium">Acessar minha conta</a>
